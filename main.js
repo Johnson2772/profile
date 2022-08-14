3d5772c83e0a5ac762b8363eec795ac689e7ea21
Jhonatan Posada <3270380+Johnson2772@users.noreply.github.com>
2022-08-14 03:56:07 -0400

    Creating main javascript file

---
+++ main.js
@@ -0,0 +1,27 @@
+function index(){
+    var titulo = document.getElementById("titulo")
+     var p = document.getElementById("p");
+     titulo.innerHTML="Welcome!!!";
+     p.innerHTML="This is my first web site in The SoloLearn...<br>You can navagation in menus...";
+  
+ }
+ function home(){
+     var titulo = document.getElementById("titulo")
+     var p = document.getElementById("p");
+     titulo.innerHTML="Hii... ";
+     p.innerHTML="Nice to meet you.. :D";
+ }
+ 
+ function imagens(){
+     var titulo = document.getElementById("titulo")
+     var p = document.getElementById("p");
+     titulo.innerHTML="In Progress... ";
+     p.innerHTML="I see you baby :D";
+ }
+ 
+ function about(){
+     var titulo = document.getElementById("titulo")
+     var p = document.getElementById("p");
+     titulo.innerHTML="About Me ... ";
+     p.innerHTML="Hello, I'm Johnson, 34 years old, I'm from Colombia (Hola mundo a todos los hispanos) This Web site is the simple exemple about Home pages, Menus and Context design <br><br> Good bye there :D";
+ }
