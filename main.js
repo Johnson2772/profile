dbb319ed82d381676a558cbe508df49872f9ae99
Jhonatan Posada <3270380+Johnson2772@users.noreply.github.com>
2022-08-16 00:55:16 -0400

    Dr. Frankstein will be happy

--- main.js
+++ main.js
@@ -25,3 +25,46 @@ function index(){
      titulo.innerHTML="About Me ... ";
      p.innerHTML="Hello, I'm Johnson, 35 years old, I'm originally from Colombia (Hola mundo a todos los hispanos leyendo esto) This Web site is the simple example about Home pages, Menus and Context design <br><br> Good bye there :D";
  }
+
+
+ $(function(){
+    $("#menuBtn").mouseover(function(){
+        var toggle = false;
+        if(toggle == false){
+            $('#menu').css("left", "0");
+            toggle = true;
+            $("#menu").trigger("mouseover");
+        }
+        else{
+            var negWidth = document.documentElement.clientWidth * -1;
+            $("#menu").css("left", negWidth);
+            toggle = false;
+        }
+    });
+    $("#menu").mouseout(function(){
+            var negWidth = document.documentElement.clientWidth * -1;
+            $("#menu").css("left", negWidth);
+            toggle = false;
+    });
+    var documentHeight = document.documentElement.clientHeight;
+    var titlebarHeight = $(".titleBar").height();
+    var mainHeight = documentHeight - titlebarHeight;
+    $("#main").css("height", mainHeight);
+    $("#main").css("top", titlebarHeight);
+    
+   $("#menu").mouseover(function(){
+            $("#menu").css("left", "0");
+            toggle = true;
+            });
+            var typeOfDevice = navigator.userAgent;
+            if(typeOfDevice.toLowerCase().search("mobile") > 0){
+               $("#main").click(function(){
+            var negWidth = document.documentElement.clientWidth * -1;
+            $("#menu").css("left", negWidth);
+            toggle = false;
+               });
+            }
+            else{
+                
+            }
+ });
