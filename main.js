e5965252f296150a5b49b5abf1a09718d2711365
Jhonatan Posada <3270380+Johnson2772@users.noreply.github.com>
2022-08-29 03:20:02 -0400

    Creating a slideshow

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* Trying to make a dinamic webpage */
+
 function index(){
     var titulo = document.getElementById("titulo")
      var p = document.getElementById("p");
@@ -27,6 +29,35 @@ function index(){
  }
 
 
+
+
+ 
+/* Slideshow  */
+ let slideIndex = [1,1];
+ let slideId = ["mySlides1", "mySlides2"]
+ showSlides(1, 0);
+ showSlides(1, 1);
+ 
+ function plusSlides(n, no) {
+   showSlides(slideIndex[no] += n, no);
+ }
+ 
+ function showSlides(n, no) {
+   let i;
+   let x = document.getElementsByClassName(slideId[no]);
+   if (n > x.length) {slideIndex[no] = 1}    
+   if (n < 1) {slideIndex[no] = x.length}
+   for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+   }
+   x[slideIndex[no]-1].style.display = "block";  
+ }
+
+
+
+
+
+/* Only god knows XD */
  $(function(){
     
     $("#item").click(function() {
