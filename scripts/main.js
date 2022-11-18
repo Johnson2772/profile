9bd8b3ba1682332810cc4c764f95a02533c72eb4
Johnson2772 <3270380+Johnson2772@users.noreply.github.com>
2022-11-18 01:45:45 -0500

    Slideshow

--- scripts/main.js
+++ scripts/main.js
@@ -183,42 +183,42 @@ var x = setInterval(() => {
 
 /* Slideshow  */
 
-// let slideIndex = [1, 1];
-// let slideId = ["mySlides1", "mySlides2"]
-// showSlides(1, 0);
-// showSlides(1, 1);
+let slideIndex = [1, 1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-// function plusSlides(n, no) {
-//   showSlides(slideIndex[no] += n, no);
-// }
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
 
-// function showSlides(n, no) {
-//   let i;
-//   let x = document.getElementsByClassName(slideId[no]);
-//   if (n > x.length) { slideIndex[no] = 1 }
-//   if (n < 1) { slideIndex[no] = x.length }
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   x[slideIndex[no] - 1].style.display = "block";
-// }
-
-
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) { slideIndex[no] = 1 }
+  if (n < 1) { slideIndex[no] = x.length }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
+}
 
 
-// /* Automatic Slideshow - change image every 3 seconds */
 
-// var myIndex = 0;
-// carousel();
 
-// function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   myIndex++;
-//   if (myIndex > x.length) { myIndex = 1 }
-//   x[myIndex - 1].style.display = "block";
-//   setTimeout(carousel, 3000);
-// }
+/* Automatic Slideshow - change image every 3 seconds */
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) { myIndex = 1 }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
