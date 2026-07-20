02e4901626eb8736b91ef24d1e2fdb64f5fb5fc8
Johnson2772 <johnson2772@gmail.com>
2026-07-20 18:23:25 -0400

    Debugging tabs

--- scripts/main.js
+++ scripts/main.js
@@ -26,10 +26,7 @@ function openPage(pageName, element) {
   }
   document.getElementById(pageName).style.display = "block";
   element.style.backgroundColor = "var(--main-color)";
-  document.getElementsByClassName(pageName)[0].style.display = "block";
-
-
-  document.getElementById("").innerHTML = `Variable name: ${pageName}`;
+  document.getElementsByClassName(pageName).style.display = "block";
 }
 
 
